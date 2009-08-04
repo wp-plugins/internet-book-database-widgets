16b8a58360641693cf6d0b993a22e5b821b4d734
ibookdb <ibookdb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-04 16:21:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/internet-book-database-widgets/tags/1.4@142762 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ibookdbwidget.php
@@ -0,0 +1,102 @@
+<?php
+/*
+Plugin Name: Internet Book Database Book Widgets
+Plugin URI: http://www.ibookdb.net/forums/index.php/topic,2054.0.html
+Description: Internet Book Database Bookshelf and Book Widgets
+Author: Siddharth Dalal
+Version: 1.4
+Author URI: http://www.ibookdb.net/forums/index.php/topic,2054.0.html
+*/
+
+function widget_iBookDB_Book($args) {
+  extract($args);
+  echo $before_widget;
+  $options = get_option('widget_iBookDB_Book');
+  if($options['isbn']) echo file_get_contents('http://www.ibookdb.net/bookbadgetest.php?isbn=' . $options['isbn']);
+  else echo 'No ISBN Selected';
+  echo $after_widget;
+}
+
+function widget_iBookDB_Bookshelf($args) {
+  extract($args);
+  echo $before_widget;
+  $options = get_option('widget_iBookDB_Bookshelf');
+  echo '<script type="text/javascript" src="http://www.ibookdb.net/blogshelf.php?username=' . $options['username'] . '&cols=' . $options['cols'] . '&limit=' . ($options['cols']*$options['rows']) . '&smallimage=' . $options['smallimage'] . '"></script>';
+  echo $after_widget;
+}
+
+function options_iBookDB_Book() {
+	$options = get_option('widget_iBookDB_Book');
+	if ($_POST['widget_iBookDB_Book-isbn']) {
+		$isbn=trim($_POST['widget_iBookDB_Book-isbn']);
+		if (strlen($isbn)==10 or strlen($isbn)==13) {
+			$newoptions['isbn']=$isbn;
+		}
+		if($options!=$newoptions) {
+			$options=$newoptions;
+			update_option('widget_iBookDB_Book', $options);
+		}
+	
+	}
+	
+	
+	$isbn=$options['isbn'];
+	
+	echo '<div>
+	<label for="widget_iBookDB_Book-isbn">ISBN: <input type="text" id="widget_iBookDB_Book-isbn" name="widget_iBookDB_Book-isbn" value="' . $isbn . '"/></label>
+	</div>';
+	
+}
+
+function options_iBookDB_Bookshelf() {
+	//echo 'options<br>';
+	$options = get_option('widget_iBookDB_Bookshelf');
+	//print_r ($_POST);
+	if ($_POST['widget_iBookDB_Bookshelf-username'] and $_POST['widget_iBookDB_Bookshelf-rows'] and $_POST['widget_iBookDB_Bookshelf-cols']) {
+		//echo 'options submitted<br>';
+		$newoptions['username']=$_POST['widget_iBookDB_Bookshelf-username'];
+		$newoptions['rows']=$_POST['widget_iBookDB_Bookshelf-rows'];
+		$newoptions['cols']=$_POST['widget_iBookDB_Bookshelf-cols'];
+		$newoptions['smallimage']=$_POST['widget_iBookDB_Bookshelf-smallimage'];
+		if($options!=$newoptions) {
+			$options=$newoptions;
+			update_option('widget_iBookDB_Bookshelf', $options);
+			echo 'options saved<br>';
+		}
+		//echo 'after options saved<br>';
+	}
+	
+	echo '<div>
+	<label for="widget_iBookDB_Bookshelf-username">Username: <input type="text" id="widget_iBookDB_Bookshelf-username" name="widget_iBookDB_Bookshelf-username" value="' . $options['username'] . '"/></label><br>
+	<label for="widget_iBookDB_Bookshelf-rows">Rows: <input type="text" id="widget_iBookDB_Bookshelf-rows" name="widget_iBookDB_Bookshelf-rows" value="' . $options['rows'] . '"/></label><br>
+	<label for="widget_iBookDB_Bookshelf-cols">Cols: <input type="text" id="widget_iBookDB_Bookshelf-cols" name="widget_iBookDB_Bookshelf-cols" value="' . $options['cols'] . '"/></label><br>
+	<label for="widget_iBookDB_Bookshelf-smallimage">Image Size: <select id="widget_iBookDB_Bookshelf-smallimage" name="widget_iBookDB_Bookshelf-smallimage">
+		<option value="1">Small Images</option>
+		<option value="0" ' . ($options['smallimage']=='0'?'selected':'') . '>Medium Images</option>
+	</select></label>
+	</div>';
+	
+}
+
+function iBookDB_init() {
+  register_sidebar_widget(__('Internet Book Database Book Widget'), 'widget_iBookDB_Book');
+  register_widget_control(__('Internet Book Database Book Widget'), 'options_iBookDB_Book');
+  
+  register_sidebar_widget(__('Internet Book Database Bookshelf Widget'), 'widget_iBookDB_Bookshelf');
+  register_widget_control(__('Internet Book Database Bookshelf Widget'), 'options_iBookDB_Bookshelf');
+}
+
+function isbn_replace($index_array) {
+	//echo $index_array[0];
+	return file_get_contents('http://www.ibookdb.net/bookbadgetest.php?isbn=' . str_replace(array('[ISBN:', ']'), '', $index_array[0]));
+}
+
+function iBookDB_ISBN($content) {
+	$content=preg_replace_callback ('/\[ISBN:([0-9xX]+)\]/' , 'isbn_replace' , $content);
+	return $content;
+}
+
+add_action("plugins_loaded", "iBookDB_init");
+add_filter('the_content', 'iBookDB_ISBN');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Internet Book Database Book Widgets ===
+Contributors: Siddharth Dalal
+Donate link: http://www.ibookdb.net/marketing.php
+Tags: Books, Bookshelf, Manage, Authors, ISBN, Sidebar, Post, Widget
+Requires at least: 2.6
+Tested up to: 2.8
+Stable tag: 1.4
+
+The Internet Book Database Bookshelf and Book Information from ISBN Widgets.
+
+== Description ==
+
+= Book Widget = 
+Get Book information, Author, Title, Cover for millions of Books by ISBN using the Book Widget. No account required. 
+
+= Book Information in Blog Posts =
+You can also insert the same book information directly into your posts by typing
+[ISBN:ISBNNUMBER] for example [ISBN:9780805079593]
+
+= Bookshelf Widget = 
+You need an account at iBookDB to use the Bookshelf widget which shows random books from your bookshelf on your blog. 
+Just enter your username, number of rows and number of columns and you are good to go. You can pick small or medium sized images.
+
+= Using The Widgets and Plugin = 
+To use the widgets go to Appearance->Widgets and drag and drop the widgets wherever you want.
+For an example on using and customization go to http://www.s1dd.com/2009/07/the-internet-book-database-book-information-widget/
+
+= Questions? =
+Visit the Plugin Forum at http://www.ibookdb.net/forums/index.php/topic,2054.0.html
+
+== Installation ==
+
+We recommend installing this plugin using the automatic installation from the Plugin Directory.
+
+This section describes how to manually install the plugin and get it working.
+
+e.g.
+
+1. Upload `ibookdbwidget.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. To use go to Appearance->Widgets and drag and drop the widgets wherever you want.
+
+== Screenshots ==
+1. Examples
+
+== Changelog ==
+= 1.4 =
+* Added Screenshots, instructions etc. No major Changes
+
+= 1.3 =
+* Added Preview Support for Book with Previews
+
+= 1.2 = 
+* Removed iBookDB theme so content matches your blog.
+
+= 1.1 = 
+* Added [ISBN:ISBNNUMBER] Filter for Posts
+
+== Frequently Asked Questions ==
+
+None so far!
